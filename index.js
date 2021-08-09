d1fc36c9e7f55d28ff5b2a02f448205f8ec91e09
Hieu <xuanhieu0012@gmail.com>
2021-08-09 12:43:11 -0500

    commit tree 2ecd7d3120e59bb0b49b439d2050b78b25367522

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 =2;
+multiply = num1 * num2;
+
+let random
+random = Math.floor(Math.random()* 10);
+
+let mod;
+let num3 = 28;
+let num4 = 6;
+mod = num3 % num4;
+let max;
+max = Math.max(0,20);
